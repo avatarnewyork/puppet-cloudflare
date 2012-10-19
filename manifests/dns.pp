235d69e7e8e2f79a0e27ef5166e0192532205078
patrick tully <patrick@avatarnewyork.com>
2012-10-19 15:14:17 -0400

    added init files

---
+++ manifests/dns.pp
@@ -0,0 +1,9 @@
+class cloudflare::dns (
+  $type = "A",
+  $name = "$hostname",
+  $content = "$ipaddress_eth0",
+  $service_mode = "0",
+  $ttl = "1",
+  ) {
+
+}
---
+++ manifests/init.pp
@@ -0,0 +1,6 @@
+import '*'
+
+class cloudflare($email, $tkn, $domain){
+  $cloudflare_api = 'https://www.cloudflare.com/api_json.html'
+  class {'cloudflare::dns':}
+}
